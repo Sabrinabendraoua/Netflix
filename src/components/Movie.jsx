7be9457fb2e8f179fb093c77e7566f203bb771aa
sabrina bendraoua <sabbendra@gmail.com>
2023-04-25 17:23:18 +0200

    create map Movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,15 +1,17 @@
 import data from "../data.json";
-
 console.log(data);
-
 const movie = () => {
   return data.map((movieSelection) => {
     return (
       <section>
         <h2>{movieSelection.category}</h2>
-        <div className="caroussel">
-          <img src="" alt="" />
-        </div>
+        {data.map((movieSelection) => {
+          return (
+            <div className="caroussel">
+              <img src={movieSelection.images} alt="" />
+            </div>
+          );
+        })}
       </section>
     );
   });
